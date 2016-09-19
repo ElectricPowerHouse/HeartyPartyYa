f044fb4b415cac0cd0e08f81760e34cf781f6b23
ElectricPowerHouse <acidplaystf2@gmail.com>
2016-09-19 23:46:24 +1200

    Folders + Resolution Setup
    It will always start at 1920 x 1080

---
+++ Attachment/Assets/Scripts/SetupScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+
+public class SetupScript : MonoBehaviour {
+
+   
+   
+
+	// Use this for initialization
+	void Start () {
+
+        // Resolution should always be 1920x1080
+        Screen.SetResolution(1920, 1080, false);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
